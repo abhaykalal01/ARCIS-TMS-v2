8820f5c04a54846df3199419a73ae17202934e2b
abhay_kalal01 <abhaykalal7@gmail.com>
2026-03-17 15:37:10 +0530

    fixed project structure and removed duplicates

--- routes/projectRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { createProject } from "../controllers/projectController.js";
-
-router.post("/" , createProject);
-
-export default router
--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { createEmployee } from "../controllers/userController.js";
-
-router.post("/" , createEmployee);
-
-export default router
