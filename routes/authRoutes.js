f7133a66a90fc89af9f82bb19a917c8525de8225
abhay_kalal01 <abhaykalal7@gmail.com>
2026-03-18 11:56:17 +0530

    ARCIS PROJECT PUSH GITHUB

--- routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { loginUser } from "../controllers/authController.js";
-
-router.post("/login" , loginUser);
-
-export default router
