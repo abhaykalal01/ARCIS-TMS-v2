1241072e6d25aa4572c8ae8ff0f169e0de2c23d8
abhay_kalal01 <abhaykalal7@gmail.com>
2026-03-20 16:59:55 +0530

    some new add

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -10,8 +10,24 @@ try {
         })
     }
     token = token.split(" ")[1];
+
+    const decoded = jwt.verify(token , process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
     
 } catch (error) {
+    res.status(401).json({
+        message : "Not authorized"
+    })
+}
+}
+
+export const isAdmin = (req , res , next) => {
+    if(req.user.role !== "admin"){
+        return res.status(403).json({
+            message : "Admin access required"
+        })
     
-}
-}
+    }
+    next();
+};
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
 import { createEmployee } from "../controllers/userController.js";
+import { isAdmin , protect } from "../middleware/authMiddleware.js"
 
-router.post("/" , createEmployee);
+router.post("/" , protect , isAdmin , createEmployee);
 
 export default router
